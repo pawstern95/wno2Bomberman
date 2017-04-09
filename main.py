7211680c0c3edc9fa160e3f62ec2d1c7fed0a47a
Paweł <sternicki.pawel1995@gmail.com>
2017-04-09 10:29:23 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,285 @@
+import numpy as np
+from enum import Enum
+import os
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtTest
+import sys
+import random
+
+# amounts on board:
+# 1 - agent
+# 2 - undestroyable stone
+# 3 - destroyable stone
+# 4 - weak bomb
+# 5 - agent with weak bomb
+# 6 - strong bomb
+# 7 - agent with strong bomb
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+class Bomb:
+    def __init__(self, scope=1):
+        self.scope = scope
+        self.time = 3 #seconds
+
+class Stone:
+    def __init__(self, toughness=True):
+        self.toughness = toughness
+
+    def putStone(self):
+        if self.toughness == True:
+            return 2
+        else:
+            return 3
+
+
+class Agent:
+    xPosition = 0
+    yPosition = 0
+    bombs3 = []
+    bombs1 = []
+    destroyed = False
+
+    def addBombs(self, amount=5, scope=1):
+        if scope == 1:
+            for i in range(0, amount):
+                self.bombs1.append(Bomb(scope=scope))
+        else:
+            for i in range(0, amount):
+                self.bombs3.append(Bomb(scope=scope))
+
+    def getPosition(self):
+        return (self.xPosition, self.yPosition)
+
+    def setPosition(self, xPos, yPos):
+        self.xPosition = xPos
+        self.yPosition = yPos
+
+    def moveRight(self):
+        x,y = self.getPosition()
+        self.setPosition(x, y+1)
+
+    def moveLeft(self):
+        x,y = self.getPosition()
+        self.setPosition(x, y-1)
+
+    def moveUp(self):
+        x,y = self.getPosition()
+        self.setPosition(x-1, y)
+
+    def moveDown(self):
+        x,y = self.getPosition()
+        self.setPosition(x+1, y)
+
+    def dropBomb1(self):
+        if self.bombs1:
+            self.bombs1.pop()
+            return 5, False
+        else:
+            return 1, True
+
+    def dropBomb3(self):
+        if self.bombs3:
+            self.bombs3.pop()
+            return 7, False
+        else:
+            return 1, True
+
+    def killAgent(self):
+        self.destroyed = True
+
+    def alive(self):
+        return self.destroyed
+
+
+class Board:
+    def __init__(self, agent, xSize = 40, ySize = 40):
+        self.xSize = xSize
+        self.ySize = ySize
+        self.board = np.zeros((xSize, ySize))
+        self.genBoard()
+        self.agent = agent
+        self.putAgent()
+
+
+    def putAgent(self):
+        x,y = self.agent.getPosition()
+        self.board[x][y] = 1
+
+    def takeOffAgent(self):
+        x, y = self.agent.getPosition()
+        self.board[x][y] -= 1
+
+    def destroyWithBomb(self, x, y, ran):
+        for i in range(0, ran+1):
+            if self.board[x][y+i] == 3 or self.board[x][y+i] == 4 or self.board[x][y+i] == 6:
+                self.board[x][y+i] = 0
+            elif self.board[x][y+i] == 1 or self.board[x][y+i] == 5 or self.board[x][y+i] == 7:
+                self.board[x][y+i] = 0
+                self.agent.killAgent()
+            elif self.board[x][y+i] == 2:
+                break
+        for i in range(0, ran + 1):
+            if self.board[x-i][y] == 3 or self.board[x-i][y] == 4 or self.board[x-i][y] == 6:
+                self.board[x-i][y] = 0
+            elif self.board[x-i][y] == 1 or self.board[x-i][y] == 5 or self.board[x-i][y] == 7:
+                self.board[x-i][y] = 0
+                self.agent.killAgent()
+            elif self.board[x-i][y] == 2:
+                break
+        for i in range(0, ran + 1):
+            if self.board[x+i][y] == 3 or self.board[x+i][y] == 4 or self.board[x+i][y] == 6:
+                self.board[x+i][y] = 0
+            elif self.board[x+i][y] == 1 or self.board[x+i][y] == 5 or self.board[x+i][y] == 7:
+                self.board[x+i][y] = 0
+                self.agent.killAgent()
+            elif self.board[x+1][y] == 2:
+                break
+        for i in range(0, ran + 1):
+            if self.board[x][y-i] == 3 or self.board[x][y-i] == 4 or self.board[x][y-i] == 6:
+                self.board[x][y-i] = 0
+            elif self.board[x][y - i] == 1 or self.board[x][y - i] == 5 or self.board[x][y - i] == 7:
+                self.board[x][y-i] = 0
+                self.agent.killAgent()
+            elif self.board[x][y - i] == 2:
+                break
+
+    def dropBomb1(self):
+        x, y = self.agent.getPosition()
+        if self.board[x][y] == 1:
+            value, empty = self.agent.dropBomb1()
+            self.board[x][y] = value
+        return x, y, empty
+
+
+    def dropBomb3(self):
+        x, y = self.agent.getPosition()
+        if self.board[x][y] == 1:
+            value, empty = self.agent.dropBomb3()
+            self.board[x][y] = value
+        return x, y, empty
+
+    def moveAgent(self, dir):
+        if dir == Direction.RIGHT:
+            x,y = self.agent.getPosition()
+            if (y < self.ySize - 1) and self.board[x][y+1] != 2 and self.board[x][y+1] != 3 and \
+                            self.board[x][y+1] != 4 and self.board[x][y+1] != 6:
+                self.takeOffAgent()
+                self.agent.moveRight()
+                self.putAgent()
+        elif dir == Direction.LEFT:
+            x, y = self.agent.getPosition()
+            if y > 0 and self.board[x][y-1] != 2 and self.board[x][y-1] != 3 and \
+                            self.board[x][y-1] != 4 and self.board[x][y-1] != 6:
+                self.takeOffAgent()
+                self.agent.moveLeft()
+                self.putAgent()
+        elif dir == Direction.UP:
+            x, y = self.agent.getPosition()
+            if x > 0 and self.board[x-1][y] != 2 and self.board[x-1][y] != 3 and \
+                            self.board[x-1][y] != 4 and self.board[x-1][y] != 6:
+                self.takeOffAgent()
+                self.agent.moveUp()
+                self.putAgent()
+        elif dir == Direction.DOWN:
+            x, y = self.agent.getPosition()
+            if x < self.xSize-1 and self.board[x+1][y] != 2 and self.board[x+1][y] != 3 and \
+                            self.board[x+1][y] != 4 and self.board[x+1][y] != 6:
+                self.takeOffAgent()
+                self.agent.moveDown()
+                self.putAgent()
+
+    def genBoard(self):
+        for x in range(0, self.xSize):
+            for y in range(0, self.ySize):
+                prob = random.randrange(0,4)
+                if prob == 0:
+                    # put undestroyable stone
+                    self.board[x][y] = Stone().putStone()
+                elif prob == 1:
+                    # put destroyable stone
+                    self.board[x][y] = Stone(False).putStone()
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.agent = Agent()
+        self.agent.addBombs()
+        self.agent.addBombs(amount=2, scope=3)
+        self.board = Board(self.agent, 30, 30)
+        self.displayBoard()
+        self.timers = []
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Event handler')
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Right:
+            self.board.moveAgent(Direction.RIGHT)
+            self.displayBoard()
+        elif e.key() == Qt.Key_Left:
+            self.board.moveAgent(Direction.LEFT)
+            self.displayBoard()
+        elif e.key() == Qt.Key_Up:
+            self.board.moveAgent(Direction.UP)
+            self.displayBoard()
+        elif e.key() == Qt.Key_Down:
+            self.board.moveAgent(Direction.DOWN)
+            self.displayBoard()
+        elif e.key() == Qt.Key_Space:
+            x, y, empty = self.board.dropBomb1()
+            if not empty:
+                self.displayBoard()
+                self.timers.append(QTimer())
+                self.timers[len(self.timers) - 1].setInterval(3000)
+                timer = self.timers[len(self.timers) - 1]
+                timer.timeout.connect(lambda: self.board.destroyWithBomb(x, y, 1))
+                timer.timeout.connect(self.displayBoard)
+                timer.timeout.connect(timer.stop)
+                timer.start()
+        elif e.key() == Qt.Key_Alt:
+            x, y, empty = self.board.dropBomb3()
+            if not empty:
+                self.displayBoard()
+                self.timers.append(QTimer())
+                self.timers[len(self.timers) - 1].setInterval(3000)
+                timer = self.timers[len(self.timers) - 1]
+                timer.timeout.connect(lambda: self.board.destroyWithBomb(x, y, 3))
+                timer.timeout.connect(self.displayBoard)
+                timer.timeout.connect(timer.stop)
+                timer.start()
+
+
+    def displayBoard(self):
+        print('\n' * 100)
+
+        if(self.board.agent.alive()):
+            print('GAME OVER')
+        else:
+            array = []
+            for x in range(0, self.board.xSize):
+                row = ''
+                for y in range(0, self.board.ySize):
+                    row += str(self.board.board[x][y])[:1] + ' '
+                array.append(row)
+            for i in range(0, len(array)):
+                print(array[i])
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = Main()
+    sys.exit(app.exec_())
+
+
+
